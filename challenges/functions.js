0a96c7e358056ac839da11b868a906f52011e5ef
colbyhowell <49887775+colbyhowell@users.noreply.github.com>
2019-05-31 20:53:43 -0500

    finished the functions task

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,6 +7,9 @@
   * In the body of the function return the callback with the two parameters that you created
 */
 
+function consume(arg1, arg2, cb){
+  return cb(arg1, arg2)
+}
 
 /* Step 2: Create several functions to callback with consume();
   * Create a function named add that returns the sum of two numbers
@@ -14,18 +17,32 @@
   * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
 */
 
+function add(a1, a2){
+  console.log(a1 + a2)
+}
+
+function multiply(a1, a2){
+  console.log(a1 * a2)
+}
+
+function greeting(a1, a2){
+  console.log(`Hello ${a1} ${a2}, it's nice to meet you`)
+}
+
 
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2,2,add); // 4
+consume(10,16,multiply); // 160
+consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+
+
 
 
 // ==== Closures ==== 
 
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
-// Explanation: 
+// Explanation: nestedFunction() can access internal because JS searches for things at the innermost scope and moves outward, but this variable is not available outside of the function. Lexical scope is when all the variables are available within the scope of the function itself.
 
 
 const external = "I'm outside the function";
